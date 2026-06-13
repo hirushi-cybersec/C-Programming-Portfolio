8dde6fea276629afaa5fd0418a47c6ce036c4ee4
hirushi-cybersec <hirushikarunasingha6@gmail.com>
2026-06-14 00:58:41 +0530

    Enhance output readability in SimpleCalculator
    Updated output formatting to include extra newlines for clarity.

--- Calculators/v1/SimpleCalculator_v1.c
+++ Calculators/v1/SimpleCalculator_v1.c
@@ -1,10 +1,14 @@
-
 #include<stdio.h>
+
 
 int main()
 {
-    printf("Welcome to Hirushi's calculator\n\n\n");
+    int x=1;
+    while (x>0)
+ {
+    printf("\n----------------------------------------\n");
     int num1,num2,operation,answer;
+
     printf("enter your number1: ");
     scanf("%d",&num1);
     printf("enter your number2: ");
@@ -23,26 +27,31 @@ int main()
     {
     case 1:
         answer=num1+num2;
-        printf("answer-addition: %d\n",answer);
+        printf("answer-addition: %d\n\n\n",answer);
         break;
     case 2:
         answer=num1-num2;
-        printf("answer-subtraction: %d\n",answer);
+        printf("answer-subtraction: %d\n\n\n",answer);
         break;
     case 3:
         answer=num1/num2;
-        printf("answer-division: %d\n",answer);
+        printf("answer-division: %d\n\n\n",answer);
         break;
     case 4:
         answer=num1*num2;
-        printf("answer-multiplication: %d\n",answer);
+        printf("answer-multiplication: %d\n\n\n",answer);
         break;
     case 5:
         answer=num1%num2;
-        printf("answer-remainder: %d\n",answer);
+        printf("answer-remainder: %d\n\n\n",answer);
         break;
     default:
-        printf("number invalid \n");
+        printf("number invalid \n\n\n");
+        x++;
     }
+ }
     return 0;
 }
+
+
+
